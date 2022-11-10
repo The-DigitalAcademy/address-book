027e4cdf7ba8aa73f2dc98a69e6cabbd2b6b57bc
tetelomas075@gmail.com <tetelomas075@gmail.com>
2022-11-10 05:54:56 -0800

    updated database

--- Address_book.db.session.sql
+++ Address_book.db.session.sql
@@ -1,18 +1,32 @@
 -- Active: 1667747711399@@127.0.0.1@5430@address_book
 CREATE TABLE details(
-    id SERIAL PRIMARY KEY NOT NULL,
+    id INT PRIMARY KEY NOT NULL,
     names VARCHAR(50) NOT NULL,
     email VARCHAR(25) NOT NULL,
-    address VARCHAR(45) NOT NULL,
-    postcode INT NOT NULL,
-    province VARCHAR NOT NULL,
     contact VARCHAR(10) NOT NULL,
-    search_count INT NOT NULL
+    
+);
+
+CREATE TABLE location(
+    street_name INT PRIMARY KEY NOT NULL,
+    house_number VARCHAR (10) NOT NULL,
+    city VARCHAR(45) NOT NULL,
+    postcode VARCHAR NOT NULL,
+    province VARCHAR(30) NOT NULL,
+    search_count INT NOT NULL,
+    CONSTRAINT fk_id
+        FOREIGN KEY(id) 
+        REFERENCES details(id)
 );
 
 
-
-INSERT INTO details(names, email, address,postcode,province,contact,search_count)
+INSERT INTO details(id,names, email,contact)
 VALUES
-('John Diale','john@gmail.com','22Top',0152,'Gauteng','0684214968',0),
-('Mbedzi Gudani','mbedzigudani@gmail.com', '111 kerk',2001,'Gauteng', '0718345255',0);
+(9420,"linah Joseph","linahjoseph@gmail.com", "0713042320"),
+(9421, "queen latifa", "quuenl@gmail.com", "0813456091")
+;
+INSERT INTO location(street_name, house_number, city, postcode, provide,)
+VALUES
+("hellen_joseph", "234", "Cape town","0157" "western cape"),
+("church_street", "123" , "polokwane", "0737", "limpopo")
+;
